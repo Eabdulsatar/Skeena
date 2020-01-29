57c6ff5396d13c1ac6f96cba86600f1c472b0fe4
Eabdulsatar <esam.abdulsatar@gmail.com>
2020-01-29 11:28:27 -0500

    Update Configuration.h
    updated park nozzle feature

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1277,7 +1277,7 @@
 
 #if ENABLED(NOZZLE_PARK_FEATURE)
   // Specify a park position as { X, Y, Z }
-  #define NOZZLE_PARK_POINT { 100 , 10 , 50 }
+  #define NOZZLE_PARK_POINT { 150 , 10 , 50 }
   #define NOZZLE_PARK_XY_FEEDRATE 100   // X and Y axes feedrate in mm/s (also used for delta printers Z axis)
   #define NOZZLE_PARK_Z_FEEDRATE 5      // Z axis feedrate in mm/s (not used for delta printers)
 #endif
